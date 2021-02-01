b7c2716c2363f1d2ae9da8b194cd4e7782f9814b
Alessandro <internicola.alessandro@gmail.com>
2021-02-01 15:26:33 +0100

    init
    Inizializzazione esercizio

---
+++ index.php
@@ -0,0 +1,49 @@
+<!-- Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+Buon appetito e buon pomeriggio :baby-yoda: -->
+
+
+<?php
+
+  $db = [
+    "faq" => [
+      "question" => "",
+      "answer" => ""
+    ],
+    "faq" => [
+      "question" => "",
+      "answer" => ""
+    ],
+    "faq" => [
+      "question" => "",
+      "answer" => ""
+    ],
+    "faq" => [
+      "question" => "",
+      "answer" => ""
+    ],
+  ];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+  <meta charset="utf-8">
+  <title>livecoding php snack</title>
+  </head>
+
+  <body>
+
+
+
+
+  </body>
+
+</html>
+
+<!-- fin -->
